266211627b3dd9a2120a444ca88ecfd8dc4f5d67
johan <stjernholm@msn.com>
2022-09-07 13:42:12 +0800

    Feature: add testapp

--- app/testapp/views.py
+++ app/testapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "testapp/index.html")
+
+
+def login_view(request):
+    pass
+
+
+def logout_view(request):
+    pass
+
+
+def register(request):
+    pass
