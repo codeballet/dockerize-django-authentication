cee3e3b043d9f010c35d73dda818c86c3f302b5a
johan <stjernholm@msn.com>
2022-09-10 00:48:00 +0800

    Refactor: factor out form objects

---
+++ app/testapp/static/testapp/forms.js
@@ -0,0 +1,61 @@
+////////////////////////
+// Form input objects //
+////////////////////////
+
+const registerFormInput = {
+    username: {
+        id: 'register_username',
+        classname: 'form_field',
+        name: 'register_username',
+        type: 'text',
+        placeholder: 'Username'
+    },
+    email: {
+        id: 'register_email',
+        classname: 'form_field',
+        name: 'register_email',
+        type: 'text',
+        placeholder: 'Email'
+    },
+    password: {
+        id: 'register_password',
+        classname: 'form_field',
+        name: 'register_password',
+        type: 'password',
+        placeholder: 'Password'
+    },
+    confirmation: {
+        id: 'register_confirmation',
+        classname: 'form_field',
+        name: 'register_confirmation',
+        type: 'password',
+        placeholder: 'Confirm password'
+    },
+    submit: {
+        id: 'register_submit',
+        classname: 'button',
+        type: 'submit'
+    }
+}
+
+const loginFormInput = {
+    username: {
+        id: 'login_username',
+        classname: 'form_field',
+        name: 'login_username',
+        type: 'text',
+        placeholder: 'Username'
+    },
+    password: {
+        id: 'login_password',
+        classname: 'form_field',
+        name: 'login_password',
+        type: 'password',
+        placeholder: 'Password'
+    },
+    submit: {
+        id: 'login_submit',
+        classname: 'button',
+        type: 'submit'
+    }
+}
