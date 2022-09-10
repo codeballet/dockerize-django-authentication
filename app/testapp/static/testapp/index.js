0bf9e9a5af1c0be9a879aacc2cba6ea965051e8f
johan <stjernholm@msn.com>
2022-09-10 17:08:16 +0800

    Refactor: factor out globals

--- app/testapp/static/testapp/index.js
+++ app/testapp/static/testapp/index.js
@@ -1,36 +1,3 @@
-////////////////////
-// Global objects //
-////////////////////
-
-// navigation menu objects
-const NAV_MENU = new NavMenu();
-const HOME_NAV_BUTTON = new NavButton('Home', 'home_nav');
-const REGISTER_NAV_BUTTON = new NavButton('Register', 'register_nav');
-const LOGIN_NAV_BUTTON = new NavButton('Login', 'login_nav');
-const LOGOUT_NAV_BUTTON = new NavButton('Logout', 'logout_nav');
-
-// page objects
-const HOME_PAGE = new Page('home_page', 'page');
-const LOGIN_PAGE = new Page('login_page', 'page');
-const REGISTER_PAGE = new Page('register_page', 'page');
-
-// forms
-const REGISTER_FORM = new InputForm(registerFormInput, 'register_page', 'register_form', 'form');
-const LOGIN_FORM = new InputForm(loginFormInput, 'login_page', 'login_form', 'form');
-
-
-//////////////////////
-// Global variables //
-//////////////////////
-
-// link nav button ids to page objects object
-PAGES = {
-    home_nav: HOME_PAGE,
-    login_nav: LOGIN_PAGE,
-    register_nav: REGISTER_PAGE,
-};
-
-
 ////////////////////////
 // DOM Content Loaded //
 ////////////////////////
